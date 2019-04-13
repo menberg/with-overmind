7c0d94cb8f16236008a4f7ccd3d896b121b9f3ed
menberg <jmenberg@web.de>
2019-04-13 20:24:57 +0200

    Get example data asynchronously via fetch request

--- overmind/actions.js
+++ overmind/actions.js
@@ -1,42 +1,40 @@
 import React from 'react'
-import data from './_data'
 
-export const initPageIndex = ({ state, actions }, count) => {
+export const initPageIndex = async ({ state, actions }, count) => {
   state.showCount = 3
-  if (!state.posts) {
-    actions.getPosts()
+  if (!state.posts || !state.posts.length) {
+    await actions.getPosts()
   }
 }
 
 export const initPage3 = initPageIndex
 
-export const initPage10 = ({ state, actions }, count) => {
+export const initPage10 = async ({ state, actions }, count) => {
   state.showCount = 10
-  if (!state.posts) {
-    actions.getPosts()
+  if (!state.posts || !state.posts.length) {
+    await actions.getPosts()
   }
 }
 
-export const initPage20 = ({ state, actions }, count) => {
+export const initPage20 = async ({ state, actions }, count) => {
   state.showCount = 20
-  if (!state.posts) {
-    actions.getPosts()
+  if (!state.posts || !state.posts.length) {
+    await actions.getPosts()
   }
 }
 
-export const initPage50 = ({ state, actions }, count) => {
+export const initPage50 = async ({ state, actions }, count) => {
   state.showCount = 50
-  if (!state.posts) {
-    actions.getPosts()
+  if (!state.posts || !state.posts.length) {
+    await actions.getPosts()
   }
 }
 
 export const getPosts = async ({ state, effects }) => {
   state.isLoadingPosts = true
-  /* state.posts = await effects.request(
+  state.posts = await effects.request(
     'https://jsonplaceholder.typicode.com/posts'
-  ) */
-  state.posts = data
+  )
   state.isLoadingPosts = false
 }
 
